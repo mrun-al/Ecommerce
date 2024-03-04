09a1828c1922d22b5105732f7c3dedf00b0979d0
MrunalU <mrunalubale99@gmail.com>
2024-03-04 22:29:58 +0530

    angular projec

---
+++ src/app/Services/product.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http: HttpClient) { }
+  getAllProduct(): Observable<any[]>{
+    return this.http.get<any[]>("http://onlinetestapi.gerasim.in/api/Ecomm/GetAllProducts");
+  }
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { CartComponent } from './Pages/cart/cart.component';
+import { SaleComponent } from './Pages/sale/sale.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "**",
+        component:HomeComponent
+    },
+    {
+        path: 'products',
+        component:HomeComponent
+    },
+    {
+        path:'cart',
+        component: CartComponent
+    },{
+        path:'sale',
+        component: SaleComponent
+    }
+];
